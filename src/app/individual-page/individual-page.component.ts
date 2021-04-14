ea3a812069b18088def6f167a21803a937463ca2
Issac To <issacto606@gmail.com>
2021-04-14 12:09:56 +0100

    Add styling and clickable

--- src/app/individual-page/individual-page.component.ts
+++ src/app/individual-page/individual-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import {HttpClient} from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-individual-page',
@@ -15,69 +16,84 @@ export class IndividualPageComponent implements OnInit {
   rankings: any
   recommendations:any
 
-  constructor(private route: ActivatedRoute,private http : HttpClient) {
+  constructor(private route: ActivatedRoute,private router: Router,private http : HttpClient) {
     this.nameInfo = "1234"
+    route.params.subscribe(val => {
+      
+        this.name = this.route.snapshot.paramMap.get('id')
+        
+        console.log("HEHEHE",this.name)
+        let data ='./assets/data/data.json'
+        this.http.get(data).toPromise().then(Response => {
+          console.log("HIHIH");
+          this.allInfo = Response
+          this.rankings = this.allInfo.slice(0,100)
+          //console.log("here",this.allInfo.slice(0,100))
+          //this.ranking = this.allInfo.slice()
+          //console.log(this.allInfo);
+          let jsonReturned = getAllInformationNeeded(this.allInfo,this.name);
+          this.nameInfo = jsonReturned
+          this.recommendations = getRecommendations(this.nameInfo,this.allInfo);
+        });
+    
+        function hideloader(){
+          document.getElementById('loading').style.display = 'none';
+        }
+    
+        function getAllInformationNeeded(info,name){
+          for(var i of info ){
+            //console.log(i.name);
+            //console.log(name);
+            //console.log(JSON.parse(i));
+            if(i.name == name){
+                console.log(i)
+                console.log(typeof i.name) 
+                return i
+            }
+          }
+          return {}
+        }
+        function getRecommendations(selectedAnime,animes){
+          var returnArray =[]
+          for(var anime of animes ){
+            var similarValues= getSimilarValues(selectedAnime.genre,anime.genre)
+            if(similarValues>0){
+              if(anime.name!=selectedAnime.name ){
+                returnArray.push([anime,similarValues])
+              }
+            }
+            returnArray.sort(function(a,b){return a[1] - b[1]});
+            returnArray.reverse();
+    
+          }
+          return returnArray.slice(0,100)
+        }
+        function getSimilarValues(selectedAnimeGenres,animeGenres){
+          var i = 0;
+          for(var genre of selectedAnimeGenres){
+            if(animeGenres.includes(genre)){
+              i+=1;
+            }
+            else{
+              i-=0.5
+            }
+          }
+          return i 
+        }
+
+
+
+      
+    })
     
    }
+   
 
-  ngOnInit(): void {
-    this.name = this.route.snapshot.paramMap.get('id')
-    
-    console.log("HEHEHE",this.name)
-    let data ='./assets/data/data.json'
-    this.http.get(data).toPromise().then(Response => {
-      console.log("HIHIH");
-      this.allInfo = Response
-      this.rankings = this.allInfo.slice(0,100)
-      //console.log("here",this.allInfo.slice(0,100))
-      //this.ranking = this.allInfo.slice()
-      //console.log(this.allInfo);
-      let jsonReturned = getAllInformationNeeded(this.allInfo,this.name);
-      this.nameInfo = jsonReturned
-      this.recommendations = getRecommendations(this.nameInfo,this.allInfo);
-    });
+  ngOnInit(): void {}
+  
+  navigate(name){
+    this.router.navigateByUrl('/individualpage/'+name);
 
-    function hideloader(){
-      document.getElementById('loading').style.display = 'none';
-    }
-
-    function getAllInformationNeeded(info,name){
-      for(var i of info ){
-        //console.log(i.name);
-        //console.log(name);
-        //console.log(JSON.parse(i));
-        if(i.name == name){
-            console.log(i)
-            console.log(typeof i.name) 
-            return i
-        }
-      }
-      return {}
-    }
-    function getRecommendations(selectedAnime,animes){
-      var returnArray =[]
-      for(var anime of animes ){
-        var similarValues= getSimilarValues(selectedAnime.genre,anime.genre)
-        if(similarValues>=2){
-          if(anime.name!=selectedAnime.name ){
-            returnArray.push([anime,similarValues])
-          }
-        }
-        returnArray.sort(function(a,b){return a[1] - b[1]});
-        returnArray.reverse();
-
-      }
-      return returnArray.slice(0,100)
-    }
-    function getSimilarValues(selectedAnimeGenres,animeGenres){
-      var i = 0;
-      for(var genre of selectedAnimeGenres){
-        if(animeGenres.includes(genre)){
-          i+=1;
-        }
-      }
-      return i 
-    }
   }
 
 }
